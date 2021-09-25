6e4f66777412f23c2683903034b58e61aa4a7f30
reschan <reschan>
2021-09-25 22:25:18 +0700

    add stuff hopefully it wont bite me

--- bot/main.py
+++ bot/main.py
@@ -26,7 +26,7 @@ ytdl_format_options = {
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
-bot = commands.Bot(command_prefix='res ')
+bot = commands.Bot(command_prefix='res ', help_command=None)
 
 
 @bot.event
@@ -34,58 +34,57 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
+@bot.event
+async def on_message(ctx):
+    if ctx.author.id == bot.user.id:
+        return None
+    await bot.process_commands(ctx)
+
+
 @bot.command(name='test')
-async def dumbstuff(ctx, *args):
-    for i in args:
-        await ctx.send(i)
+async def dumbstuff(ctx, m):
+    for i in range(50):
+        await ctx.send(m)
     print(ctx.message.content)
 
 
-"""
-        @self.bot.command(name='timer')
-        async def test(ctx, t):
-            timerins = Timer(tx, int(t))
-            timerins.remindchest.start()
-
-        @self.bot.command(name='gamble')
-        async def gambler(ctx, minroll=0, maxroll=100):
-            return await ctx.send(f"You've rolled {random.randint(minroll, maxroll)}!")
-
-        
+def parse_playlist_link(url):
+    # url =  https://www.youtube.com/watch?v=Atp5xTJS3gU&list=PLp4K2TWhRg0ANsGA7ENtr2ZjO2gvvXMJM&index=13
+    # res =  https://www.youtube.com/playlist?list=PLp4K2TWhRg0ANsGA7ENtr2ZjO2gvvXMJM&index=13
+    res = url[:url.find('watch?v=')] + 'playlist?' + url[url.find('&list=') + 1:]
+    return res
 
 
-class Timer:
-    def __init__(self, ctx, minute, note=None):
-        self.minute = minute
-        self.ctx = ctx
-        self.note = note
+async def get_videoinfo(url, author):
+    res = []
+    if '&list=' in url and 'watch?v=' in url:
+        url = parse_playlist_link(url)
 
-    @tasks.loop(minutes=1)
-    async def remindchest(self):
-        self.minute -= 1
-        if self.minute == 0:
-            self.remindchest.stop()
-        print(f'remaining {self.minute}')
-
-    @remindchest.before_loop
-    async def before_timer(self):
-        await self.ctx.send(f'I will remind you in {self.minute} minute(s), master!')
-
-    @remindchest.after_loop
-    async def after_timer(self):
-        await self.ctx.send(f'Timer ended, master {self.ctx.author.mention}!')
-        if self.note is not None:
-            await self.ctx.send(f'Timer note: {self.note}')
-"""
+    data = await bot.loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+    if 'entries' in data:
+        for i in range(len(data['entries'])):
+            res.append({})
+            res[i]['url'] = data['entries'][i].get('webpage_url')
+            res[i]['title'] = data['entries'][i].get('title')
+            res[i]['author'] = author
+            pass
+    else:
+        res.append({})
+        res[0]['url'] = url
+        res[0]['title'] = data.get('title')
+        res[0]['author'] = author
+    return res
 
 
-# class HelloWorld(commands.Cog):
-#     def __init__(self, bot):
-#         self.bot = bot
-
-#     @commands.command()
-#     async def hello(self, ctx):
-#         await ctx.send(f'Hello, {ctx.author.mention}')
+def vc_status():
+    async def predicate(ctx):
+        if not ctx.voice_client:
+            await ctx.send('ERROR: Not in a Voice Channel!')
+            return False
+        if not ctx.author.voice:
+            await ctx.send('ERROR: You are not in a Voice Channel!')
+        return ctx.voice_client and ctx.author.voice
+    return commands.check(predicate)
 
 
 class MusicPlayer(commands.Cog):
@@ -94,24 +93,20 @@ class MusicPlayer(commands.Cog):
         self.queue = []
         self.is_vc = False
 
-    def vc_status():
-        async def predicate(ctx):
-            if not ctx.voice_client:
-                await ctx.send('ERROR: Not in a Voice Channel!')
-                return False
-            return ctx.voice_client
-        return commands.check(predicate)
+        self.play_lock = asyncio.Lock()
+        self.queue_lock = asyncio.Lock()
 
     async def playqueue(self, ctx):
-        while not ctx.voice_client.is_playing() and self.queue:
-            if ctx.voice_client.is_playing():
-                await asyncio.sleep(1)
-                continue
-            ctx.voice_client.stop()
-            player = await self.YTDLSource.from_url(self.queue[0], loop=self.bot.loop, stream=True)
-            await ctx.send(f'Playing: {player.title}')
-            ctx.voice_client.play(player, after=lambda d: self.queue.pop(0))
-            print('loaded')
+        async with self.queue_lock:
+            while self.is_vc and self.queue:
+                if ctx.voice_client.is_playing() or ctx.voice_client.is_paused():
+                    await asyncio.sleep(1)
+                    continue
+                ctx.voice_client.stop()
+                player = await self.YTDLSource.from_url(self.queue[0]['url'], loop=self.bot.loop, stream=True)
+                await ctx.send(f'Playing: {player.title}')
+                ctx.voice_client.play(player, after=lambda d: self.queue.pop(0))
+            return True
 
     @commands.command()
     async def join(self, ctx):
@@ -121,51 +116,79 @@ class MusicPlayer(commands.Cog):
         if not ctx.author.voice:
             await ctx.send('ERROR: The user is not connected to a Voice Channel.')
             print('author vc not found')
-            return None
+            return False
         channel = ctx.author.voice.channel
-        await channel.connect()
         self.is_vc = True
         await ctx.send(f'Successfully connected to: {ctx.author.voice.channel}')
+        await channel.connect()
+        return True
 
     @commands.command(name='dc')
     @vc_status()
     async def leave(self, ctx):
         self.is_vc = False
         self.queue = []
-        await ctx.voice_client.disconnect()
+        return await ctx.voice_client.disconnect()
 
     @commands.command()
-    @vc_status()
     async def play(self, ctx, url):
-        self.queue.append(url)
-        await ctx.send('Added url to queue!')
+        async with self.play_lock:
+            if not self.is_vc:
+                await self.join(ctx)
+            self.queue = [*self.queue, *await get_videoinfo(url, ctx.message.author)]
+            await ctx.send('Added url to queue!')
         if not ctx.voice_client.is_playing():
-            await self.playqueue(ctx)
+            return await self.playqueue(ctx)
+        return True
 
     @commands.command()
     @vc_status()
     async def pause(self, ctx):
-        ctx.voice_client.pause()
         await ctx.send('Paused')
+        return ctx.voice_client.pause()
 
     @commands.command()
     @vc_status()
     async def resume(self, ctx):
-        ctx.voice_client.resume()
         await ctx.send('Resumed')
+        return ctx.voice_client.resume()
 
     @commands.command()
     @vc_status()
     async def stop(self, ctx):
-        self.queue = []
-        ctx.voice_client.stop()
         await ctx.send('Queue cleared and stopped')
+        self.queue = []
+        return ctx.voice_client.stop()
 
     @commands.command()
     @vc_status()
     async def skip(self, ctx):
-        ctx.voice_client.stop()
         await ctx.send('Skipped')
+        return ctx.voice_client.stop()
+
+    @commands.command()
+    @vc_status()
+    async def queue(self, ctx, entry=0):
+        embed = discord.Embed(title=f"{ctx.voice_client.channel}'s queue:", color=0x9a3eae)
+        for i in range(entry, entry + 10):
+            if i < len(self.queue):
+                embed.add_field(name=f"Requested by {self.queue[i]['author']}", value=f"{'Now playing:' if i == 0 else f'{i}.'} [{self.queue[i]['title']}]({self.queue[i]['url']})", inline=False)
+        await ctx.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if before.channel is None:
+            voice = after.channel.guild.voice_client
+            time = 0
+            while True:
+                await asyncio.sleep(1)
+                time = time + 1
+                if voice.is_playing() and not voice.is_paused():
+                    time = 0
+                if time == 180:
+                    await voice.disconnect()
+                if not voice.is_connected():
+                    break
 
     @commands.command()
     @vc_status()
