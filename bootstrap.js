bc26e679f3b61c53ec66a5a64f3508c4e6fcb2d1
reschan <reschan>
2021-09-28 22:54:59 +0700

    h

--- bootstrap.js
+++ bootstrap.js
@@ -2,16 +2,6 @@
 const fs = require("fs")
 const fetch = require("node-fetch")
 
-let application = fs.readFileSync("./application.yml", "utf8")
-
-if (process.env.PORT) {
-    application = application.replace("DYNAMICPORT", process.env.PORT)
-}
-
-if (process.env.PASS) {
-    application = application.replace("youshallnotpass", process.env.PASS)
-}
-fs.writeFileSync("./application.yml", application)
 
 const download = function (url, dest, cb) {
     const file = fs.createWriteStream(dest);
